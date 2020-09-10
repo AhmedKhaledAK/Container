f2713114503dd3a1d99d6f6120f6f3fadf773bfb
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-09-10 16:21:24 +0200

    method child

--- container.go
+++ container.go
@@ -15,7 +15,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-	
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
@@ -32,5 +33,21 @@ func run() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/new_root/u_container")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
 	cmd.Run()
 }
